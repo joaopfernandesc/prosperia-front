9a43920c82b9178d15b2bd43c4e9d2ed8802fb07
João <joaopfernandesc@gmail.com>
2020-07-26 18:24:55 -0300

    Reading QR Code page

---
+++ src/pages/QRCode/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import QrReader from 'react-qr-reader';
+import { FiChevronLeft } from "react-icons/fi";
+
+import "./styles.scss";
+
+const QRCode: React.FC = () => {
+  const [result, setResult] = useState("No result")
+
+  const handleScan = (data: string | null) => {
+    if (data) {
+      setResult(data)
+    }
+  }
+  const handleError = (err: any) => {
+    console.error(err)
+  }
+
+  return (
+    <div className="qr-code">
+      <nav className="topbar">
+        <div>
+          <FiChevronLeft/>
+        </div>
+        <div>
+          AUTORIZAÇÃO DE PAGAMENTO
+        </div>
+        <div>
+
+        </div>
+      </nav>
+
+      <div className="info-text">
+        ESCANEIE O CÓDIGO OU DIGITE O NÚMERO DE AUTORIZAÇÃO.
+      </div>
+
+      <QrReader className="frame"
+        delay={300}
+        onError={(err) => handleError(err)}
+        onScan={(data) => handleScan(data)}
+      />
+
+      <div>
+        <input type="text" className="qr-input" placeholder="Código de autorização"/>
+      </div>
+
+      <button className="qr-button">
+        CONTINUAR
+      </button>
+  </div>)
+}
+
+export default QRCode;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,14 @@ import { Route, BrowserRouter } from "react-router-dom";
 // Importando as páginas
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard/";
+import QRCode from "./pages/QRCode/";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route component={Home} path="/" exact />
       <Route component={Dashboard} path="/dashboard" />
+      <Route component={QRCode} path = "/qr_code" />
     </BrowserRouter>
   );
 };
